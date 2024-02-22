9a3ad6db44c67fc7d29a45ebb01e83d3aae4cabb
xjq <lebnizjoan@gmail.com>
2024-02-23 00:56:41 +0800

    update

--- src/main/java/com/stu/forum/ForumApplication.java
+++ src/main/java/com/stu/forum/ForumApplication.java
@@ -1,15 +1,15 @@
-package org.stu;
+package com.stu.forum;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("org.stu")
-public class Application {
+@MapperScan("com.stu.forum.mapper")
+public class ForumApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApplication.class, args);
+	}
 
 }
--- src/main/java/org/stu/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package org.stu;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserMapper extends BaseMapper<UserPO> {
-}
