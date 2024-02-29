63a18b6f1760cade5f793c045b6527d3478122de
xjq <lebnizjoan@gmail.com>
2024-02-29 23:49:12 +0800

    update

--- src/main/java/com/stu/forum/web/article/ArticleController.java
+++ src/main/java/com/stu/forum/web/article/ArticleController.java
@@ -2,12 +2,12 @@ package com.stu.forum.web.article;
 
 import com.stu.forum.core.result.GlobalResult;
 import com.stu.forum.entity.Article;
+import com.stu.forum.service.ArticleService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/article")
